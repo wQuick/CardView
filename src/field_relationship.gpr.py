c0bc14317c90bed46d09d48889a541a37e73c111
wQuick <wquick@gmail.com>
2023-12-15 23:11:41 -0300

    Revert "Merge branch 'master' of https://github.com/wQuick/CardView"
    This reverts commit 933523dd3cb0e628bcfee7ddf06084b3ba92b936, reversing
    changes made to b3925e53166fbb9d0d7a0b6ba529c601313d311a.

--- src/field_relationship.gpr.py
+++ src/field_relationship.gpr.py
@@ -1,39 +1,3 @@
-<<<<<<< Updated upstream
-#
-# Gramps - a GTK+/GNOME based genealogy program
-#
-# Copyright (C) 2021-2022  Christopher Horn
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-
-register(
-    GENERAL,
-    id="field_relationship",
-    name=_("Couple Relationship Field Calculator"),
-    description=_("A couple relationship field calculator."),
-    category="FIELD",
-    version="0.9",
-    gramps_target_version="5.2",
-    status=STABLE,
-    fname="field_relationship.py",
-    load_on_reg=True,
-    authors=["Christopher Horn"],
-    authors_email=["https://gramps-project.org"],
-)
-=======
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
@@ -68,4 +32,3 @@ register(
     authors=["Christopher Horn"],
     authors_email=["https://gramps-project.org"],
 )
->>>>>>> Stashed changes
